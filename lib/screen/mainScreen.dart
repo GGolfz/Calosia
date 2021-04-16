42233f8e5f7e4ec464fcead4dae46c157f8bfcc5
GGolfz <wisarutgolf11@gmail.com>
2021-04-16 23:54:27 +0700

    refactor: main screen :hammer:

--- lib/screen/mainScreen.dart
+++ lib/screen/mainScreen.dart
@@ -47,6 +47,54 @@ class MainScreen extends StatelessWidget {
     }
   }
 
+  void buildBottomModalSheet(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        barrierColor: Colors.transparent,
+        builder: (ctx) => Container(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                children: [
+                  Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                    ElevatedButton.icon(
+                        onPressed: () async {
+                          final _pickedImage = await imagePicker.getImage(
+                            source: ImageSource.camera,
+                            imageQuality: 70,
+                            maxWidth: 600,
+                            maxHeight: 500,
+                          );
+                          if (_pickedImage != null) {
+                            predict(_pickedImage, context);
+                          }
+                        },
+                        icon: Icon(Icons.camera_alt),
+                        label: Text('Take a photo'))
+                  ]),
+                  Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                    ElevatedButton.icon(
+                        onPressed: () async {
+                          final _pickedImage = await imagePicker.getImage(
+                            source: ImageSource.gallery,
+                            imageQuality: 70,
+                            maxWidth: 600,
+                            maxHeight: 500,
+                          );
+                          if (_pickedImage != null) {
+                            predict(_pickedImage, context);
+                          }
+                        },
+                        icon: Icon(Icons.photo_library),
+                        label: Text('Select from library'))
+                  ])
+                ],
+              ),
+              height: 150,
+            ));
+  }
+
+  void onSearch(String searchVal) {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,66 +107,14 @@ class MainScreen extends StatelessWidget {
         Container(
           color: Color(0xFFFFF9F6),
           child: Column(children: [
-            Container(
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                child: Theme(
-                  data: Theme.of(context)
-                      .copyWith(splashColor: Colors.transparent),
-                  child: TextField(
-                    onEditingComplete: () {
-                      Navigator.of(context).pushNamed('/food',
-                          arguments: {'detail': _text.text});
-                      _text.text = '';
-                      FocusScope.of(context).unfocus();
-                    },
-                    controller: _text,
-                    autofocus: false,
-                    style: TextStyle(fontSize: 20.0, color: Color(0xFF8B8B8B)),
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      prefixIcon: Icon(
-                        Icons.search,
-                        color: Color(0xFF8B8B8B),
-                      ),
-                      contentPadding: const EdgeInsets.only(
-                          left: 14.0, bottom: 8.0, top: 8.0),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white),
-                        borderRadius: BorderRadius.circular(25.7),
-                      ),
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white),
-                        borderRadius: BorderRadius.circular(25.7),
-                      ),
-                    ),
-                  ),
-                )),
+            SearchField(text: _text, onSearch: onSearch),
             Container(
                 height: 600,
                 child: ListView.separated(
-                    itemBuilder: (ctx, index) => ListTile(
-                          leading: Container(
-                            child: Image.network(
-                              'https://upload.wikimedia.org/wikipedia/commons/e/e8/Tom_yam_kung_maenam.jpg',
-                              fit: BoxFit.cover,
-                            ),
-                            width: 50,
-                            clipBehavior: Clip.hardEdge,
-                            height: 50,
-                            decoration: BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(25))),
-                          ),
-                          title: Text('Food $index'),
-                          subtitle: Text('300 kcals'),
-                          trailing: Icon(Icons.chevron_right),
-                          onTap: () {
-                            print(index);
-                            Navigator.of(context).pushNamed('/food',
-                                arguments: {'detail': '$index'});
-                          },
-                        ),
+                    itemBuilder: (ctx, index) => FoodTile(
+                        name: 'Food $index',
+                        imgURL: '$index',
+                        calories: '${index * 200}'),
                     separatorBuilder: (ctx, index) => Divider(),
                     itemCount: 10))
           ]),
@@ -131,58 +127,81 @@ class MainScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.camera_alt),
         onPressed: () {
-          showModalBottomSheet(
-              context: context,
-              barrierColor: Colors.transparent,
-              builder: (ctx) => Container(
-                    padding: EdgeInsets.symmetric(horizontal: 24),
-                    child: Column(
-                      children: [
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              ElevatedButton.icon(
-                                  onPressed: () async {
-                                    final _pickedImage =
-                                        await imagePicker.getImage(
-                                      source: ImageSource.camera,
-                                      imageQuality: 70,
-                                      maxWidth: 600,
-                                      maxHeight: 500,
-                                    );
-                                    if (_pickedImage != null) {
-                                      predict(_pickedImage, context);
-                                    }
-                                  },
-                                  icon: Icon(Icons.camera_alt),
-                                  label: Text('Take a photo'))
-                            ]),
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              ElevatedButton.icon(
-                                  onPressed: () async {
-                                    final _pickedImage =
-                                        await imagePicker.getImage(
-                                      source: ImageSource.gallery,
-                                      imageQuality: 70,
-                                      maxWidth: 600,
-                                      maxHeight: 500,
-                                    );
-                                    if (_pickedImage != null) {
-                                      predict(_pickedImage, context);
-                                    }
-                                  },
-                                  icon: Icon(Icons.photo_library),
-                                  label: Text('Select from library'))
-                            ])
-                      ],
-                    ),
-                    height: 150,
-                  ));
+          buildBottomModalSheet(context);
         },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
+class SearchField extends StatelessWidget {
+  final text;
+  final onSearch;
+  SearchField({required this.text, required this.onSearch});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: Theme(
+          data: Theme.of(context).copyWith(splashColor: Colors.transparent),
+          child: TextField(
+            onEditingComplete: () {
+              onSearch(text.text);
+              text.text = '';
+              FocusScope.of(context).unfocus();
+            },
+            controller: text,
+            autofocus: false,
+            style: TextStyle(fontSize: 20.0, color: Color(0xFF8B8B8B)),
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              prefixIcon: Icon(
+                Icons.search,
+                color: Color(0xFF8B8B8B),
+              ),
+              contentPadding:
+                  const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+                borderRadius: BorderRadius.circular(25.7),
+              ),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+                borderRadius: BorderRadius.circular(25.7),
+              ),
+            ),
+          ),
+        ));
+  }
+}
+
+class FoodTile extends StatelessWidget {
+  String name;
+  String imgURL;
+  String calories;
+  FoodTile({required this.name, required this.imgURL, required this.calories});
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Container(
+        child: Image.network(
+          imgURL,
+          fit: BoxFit.cover,
+        ),
+        width: 50,
+        clipBehavior: Clip.hardEdge,
+        height: 50,
+        decoration:
+            BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(25))),
+      ),
+      title: Text(name),
+      subtitle: Text('$calories kcals'),
+      trailing: Icon(Icons.chevron_right),
+      onTap: () {
+        Navigator.of(context).pushNamed('/food', arguments: {'detail': name});
+      },
     );
   }
 }
